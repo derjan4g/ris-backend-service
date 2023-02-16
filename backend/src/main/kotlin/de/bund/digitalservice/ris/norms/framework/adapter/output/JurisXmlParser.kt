0dae8ff5e64abf46ea2d2a3d32d9b60187776adb
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-02-16 07:59:09 +0100

    WIP

--- backend/src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/output/JurisXmlParser.kt
+++ backend/src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/output/JurisXmlParser.kt
@@ -4,11 +4,13 @@ import de.bund.digitalservice.ris.norms.application.port.output.ParseJurisXmlOut
 import de.bund.digitalservice.ris.norms.domain.entity.Norm
 import de.bund.digitalservice.ris.norms.domain.value.UndefinedDate
 import de.bund.digitalservice.ris.norms.juris.importer.importData
+import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import java.time.LocalDate
 import java.util.UUID
 import de.bund.digitalservice.ris.norms.juris.importer.model.Norm as NormData
 
+@Component
 class JurisXmlParser() : ParseJurisXmlOutputPort {
     override fun parseJurisXml(command: ParseJurisXmlOutputPort.Command): Mono<Norm> {
         val data = importData(command.zipFile)
--- backend/src/test/kotlin/utils/ReflectionAssertor.kt
+++ backend/src/test/kotlin/utils/ReflectionAssertor.kt
@@ -1,13 +1,9 @@
 package utils
 
 import de.bund.digitalservice.ris.norms.application.port.input.EditNormFrameUseCase
-import de.bund.digitalservice.ris.norms.application.port.input.ImportNormUseCase
 import de.bund.digitalservice.ris.norms.domain.entity.Norm
-import de.bund.digitalservice.ris.norms.domain.value.UndefinedDate
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.controller.EditNormFrameController
-import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.controller.ImportNormController
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.decodeLocalDate
-import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.decodeUndefinedDate
 import org.assertj.core.api.Assertions.assertThat
 import java.time.LocalDate
 import kotlin.reflect.full.memberProperties
@@ -64,60 +60,6 @@ fun assertNormAndEditNormFrameProperties(
             normMembers.find { normMember -> normFramePropertiesMember.name == normMember.name }
         assertThat(normFramePropertiesMember.get(normFrameProperties)).isEqualTo(found?.get(norm))
     }
-}
-
-fun assertNormDataAndImportNormRequestSchemaWithoutArticles(
-    normData: ImportNormUseCase.NormData,
-    importNormRequestSchema: ImportNormController.NormRequestSchema,
-) {
-    val normDataMembers = ImportNormUseCase.NormData::class.memberProperties
-    val importNormRequestSchemaMembers =
-        ImportNormController.NormRequestSchema::class.memberProperties
-    normDataMembers.filter { it.name != "articles" }.forEach { normDataMember ->
-        val found =
-            importNormRequestSchemaMembers.find { importNormRequestSchemaMember ->
-                normDataMember.name == importNormRequestSchemaMember.name
-            }
-        when (val normDataMemberValue = normDataMember.get(normData)) {
-            is LocalDate ->
-                assertThat(normDataMemberValue)
-                    .isEqualTo(
-                        decodeLocalDate(found?.get(importNormRequestSchema).toString()),
-                    )
-
-            is UndefinedDate ->
-                assertThat(normDataMemberValue)
-                    .isEqualTo(
-                        decodeUndefinedDate(
-                            found?.get(importNormRequestSchema).toString(),
-                        ),
-                    )
-
-            else -> {
-                assertThat(normDataMemberValue).isEqualTo(found?.get(importNormRequestSchema))
-            }
-        }
-    }
-}
-
-fun assertNormAndNormDataWithoutArticles(norm: Norm, normData: ImportNormUseCase.NormData) {
-    val normMembers = Norm::class.memberProperties
-    val normDataMembers = ImportNormUseCase.NormData::class.memberProperties
-    normMembers
-        .filter { it.name !in listOf("articles", "guid", "eli") }
-        .forEach { normMember ->
-            val found =
-                normDataMembers.find { normDataMember -> normMember.name == normDataMember.name }
-            when (val normMemberValue = normMember.get(norm)) {
-                is LocalDate ->
-                    assertThat(normMemberValue)
-                        .isEqualTo(decodeLocalDate(found?.get(normData).toString()))
-
-                else -> {
-                    assertThat(normMemberValue).isEqualTo(found?.get(normData))
-                }
-            }
-        }
 }
 
 fun assertNormsWithoutArticles(

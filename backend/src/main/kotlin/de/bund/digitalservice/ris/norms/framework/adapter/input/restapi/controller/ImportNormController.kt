b1bda6e81785a1297c5ac7c01ea2f409e91d0e11
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2023-04-12 08:36:02 +0200

    Migration Norms | Prototype
    Add new endpoint for import/migrate norms one-by-one using the XML files instead of a ZIP one
    
    This new versions of the juris converter contains a bug fix for the extraction of the unofficial reference

--- backend/src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/input/restapi/controller/ImportNormController.kt
+++ backend/src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/input/restapi/controller/ImportNormController.kt
@@ -3,15 +3,24 @@ package de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.control
 import de.bund.digitalservice.ris.norms.application.port.input.ImportNormUseCase
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.ApiConfiguration
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.encodeGuid
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 import java.util.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 @RestController
 @RequestMapping(ApiConfiguration.API_NORMS_PATH)
@@ -27,6 +36,44 @@ class ImportNormController(private val importNormService: ImportNormUseCase) {
             .map { data -> ResponseSchema.fromUseCaseData(data) }
             .map { body -> ResponseEntity.status(201).body(body) }
             .onErrorReturn(ResponseEntity.internalServerError().build())
+    }
+
+    @PostMapping(path = ["/migrate"])
+    fun migrateNorm(@RequestPart files: Flux<FilePart>): Mono<ResponseEntity<ResponseSchema>> {
+        return files
+            .flatMap { filepart ->
+                filepart.content().flatMap { dataBuffer: DataBuffer ->
+                    val bytes = ByteArray(dataBuffer.readableByteCount())
+                    dataBuffer.read(bytes)
+                    DataBufferUtils.release(dataBuffer)
+                    Mono.just(filepart.filename() to bytes)
+                }
+            }
+            .collectList()
+            .flatMap {
+                val zip = zipBytes(it)
+                // TODO how to name the zip file?
+                val command = ImportNormUseCase.Command(zip, "norm.zip", zip.size.toLong())
+                importNormService.importNorm(command)
+            }
+            .map { data -> ResponseSchema.fromUseCaseData(data) }
+            .map { body -> ResponseEntity.status(201).body(body) }
+            .onErrorReturn(ResponseEntity.internalServerError().build())
+    }
+
+    @Throws(IOException::class)
+    fun zipBytes(files: MutableList<Pair<String, ByteArray>>): ByteArray {
+        val baos = ByteArrayOutputStream()
+        val zos = ZipOutputStream(baos)
+        files.forEach {
+            val entry = ZipEntry(it.first)
+            entry.size = it.second.size.toLong()
+            zos.putNextEntry(entry)
+            zos.write(it.second)
+        }
+        zos.closeEntry()
+        zos.close()
+        return baos.toByteArray()
     }
 
     data class ResponseSchema private constructor(val guid: String) {

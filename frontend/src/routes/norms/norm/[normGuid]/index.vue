08838b5b6380b554e70eb47b84c3dfa89a7df55e
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2023-08-24 14:55:49 +0200

    Fix: WIP adjust components, tests to updated types
    This fixes all TypeScript complaints resulting from changes made in the
    previous commit.
    
    ⚠️ Fair warning: This is a WIP to hand over some work I started, and in
    all likelihood *badly broken*. Among other things it is entirely
    untested as required changes are not yet implemented in the backend. It
    also assumes that documentation of a norm contains *only* document
    sections on the first level, and *only* articles on the second level.
    Something similar used to be the case, so the current implementation is
    an MVP of sorts, but it's no longer true for the new data structure and
    needs to be updated soon.
    
    RISDEV-2315

--- frontend/src/routes/norms/norm/[normGuid]/index.vue
+++ frontend/src/routes/norms/norm/[normGuid]/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia"
+import { isDocumentSection, isArticle } from "@/domain/norm"
 import { useLoadedNormStore } from "@/stores/loadedNorm"
 
 const store = useLoadedNormStore()
@@ -14,39 +15,41 @@ const { loadedNorm } = storeToRefs(store)
           loadedNorm.metadataSections?.NORM?.[0]?.OFFICIAL_LONG_TITLE?.[0] ?? ""
         }}
       </h1>
-      <div v-for="section in loadedNorm.sections" :key="section.guid">
+      <div v-for="doc in loadedNorm.documentation" :key="doc.guid">
         <h2
           class="mt-40 text-center"
           :class="[
-            ['Eingangsformel', 'Schlussformel'].includes(section.designation)
+            isDocumentSection(doc) &&
+            doc.marker &&
+            ['Eingangsformel', 'Schlussformel'].includes(doc.marker)
               ? 'ds-label-01-bold mb-16'
               : 'ds-label-01-reg',
           ]"
         >
-          {{ section.designation }}
+          {{ doc.marker }}
         </h2>
-        <h2
-          v-if="section.header != null"
-          class="ds-label-01-bold mb-16 text-center"
-        >
-          {{ section.header }}
+        <h2 v-if="doc.heading" class="ds-label-01-bold mb-16 text-center">
+          {{ doc.heading }}
         </h2>
 
-        <div
-          v-for="paragraph in section.paragraphs"
-          :key="paragraph.guid"
-          class="mb-24"
-        >
-          <!-- eslint-disable vue/no-v-html -->
-          <p
-            v-html="
-              paragraph.marker === null
-                ? paragraph.text
-                : paragraph.marker + ' ' + paragraph.text
-            "
-          />
-          <!-- eslint-enable vue/no-v-html -->
-        </div>
+        <template v-if="isDocumentSection(doc)">
+          <div
+            v-for="article in doc.documentation"
+            :key="article.guid"
+            class="mb-24"
+          >
+            <!-- eslint-disable vue/no-v-html -->
+            <p
+              v-if="isArticle(article)"
+              v-html="
+                article.marker === null
+                  ? article.text
+                  : article.marker + ' ' + article.text
+              "
+            />
+            <!-- eslint-enable vue/no-v-html -->
+          </div>
+        </template>
       </div>
     </div>
   </div>
